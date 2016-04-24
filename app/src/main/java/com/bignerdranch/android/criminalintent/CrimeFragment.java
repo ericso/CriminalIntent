f7f8e71d3a7783c1e2b6b8a05bd8a9388ebc2f5b
Eric So <me@eric.so>
2016-04-24 18:42:46 +0000

    DatePickerFragment dialog returns selected date to calling fragment, writes new date to model, and updates UI.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,9 +14,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -24,6 +26,8 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int REQUEST_DATE = 0;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -72,16 +76,16 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        DateFormat dateFormat = new DateFormat();
-        CharSequence date = dateFormat.format("EEEE LLL d, yyy", mCrime.getDate());
-
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(date);
+
+        updateDate();
+
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });
@@ -97,5 +101,24 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        DateFormat dateFormat = new DateFormat();
+        CharSequence fmtDate = dateFormat.format("EEEE LLL d, yyy", mCrime.getDate());
+        mDateButton.setText(fmtDate);
     }
 }
